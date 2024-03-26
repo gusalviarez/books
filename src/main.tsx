ac2bfac57d323e7e64176f40fa601535a9ea89c3
gusalviarez <gusalv2001@gmail.com>
2024-03-25 22:46:02 -0400

    fix no null error at main

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
